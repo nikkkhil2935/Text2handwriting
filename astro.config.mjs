06c9f8494a21a6384ca573a1114bff71b5572528
nikkkhil2935 <nikhilpatil29032005@gmail.com>
2026-06-16 00:43:29 +0530

    bing update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://texttohandwriting.me',
+  trailingSlash: 'never',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
